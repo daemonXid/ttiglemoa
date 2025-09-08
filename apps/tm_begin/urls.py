e11e962857352847b62ad577cbcc35ef74f7818f
kijin lee <lqizhen@naver.com>
2025-09-08 14:43:06 +0900

    'stock_news_section

--- apps/tm_begin/urls.py
+++ apps/tm_begin/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import test_4views
 
 
 app_name = 'tm_begin'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("news/", views.investing_news, name="stock_news"),
+    # path("news/", okay_views.stock_news, name="stock_news"),
+
 ]

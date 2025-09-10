5774b019ecec44bd1031d80da1d802e6da8210d4
Xid <daemonxid@gmail.com>
2025-09-10 11:15:34 +0900

    refactor(account): tm_account 코드 분리 및 구조개선 (진행중)

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Media files (User-uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

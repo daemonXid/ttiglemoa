76ac404bbe8bb429d6875bcd5c246e73d82ba237
Xid <daemonxid@gmail.com>
2025-09-14 03:28:59 +0900

    feat: 'about' 및 'stock news' 페이지 추가

--- apps/tm_begin/views.py
+++ apps/tm_begin/views.py
@@ -77,12 +77,12 @@ def investing_news(request):
         "hero_item": hero_item,
         "grid_items": grid_items,
     }
-    return render(request, "tm_begin/each_pages/stock_news.html", ctx)
+    return render(request, "tm_begin/stock_news.html", ctx)
 
 
 
 def about(request):
-    return render(request, "tm_begin/each_pages/about.html")
+    return render(request, "tm_begin/about.html")
 
 
 

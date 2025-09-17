704037fe952c8a72fb6c2db54a20d20ea2272771
kijin lee <lqizhen@naver.com>
2025-09-17 16:01:49 +0900

    clean shomething no need code

--- static/js/main.js
+++ static/js/main.js
@@ -17,38 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-
-
-// phase 2
-// document.addEventListener("DOMContentLoaded", () => {
-//     const searchForm = document.querySelector("form[role='search']");
-//     const searchInput = document.querySelector("input[name='q']");
-
-//     if (searchForm && searchInput) {
-//         searchForm.addEventListener("submit", () => {
-//         setTimeout(() => {
-//             searchInput.value = "";   // 제출 직후 input 비우기
-//         }, 10);
-//         });
-//     }
-// });
-
-// phase 1
-// document.addEventListener("DOMContentLoaded", () => {
-//   const searchInput = document.querySelector("input[name='q']");
-//   if (searchInput) {
-//     const originalPlaceholder = searchInput.getAttribute("placeholder");
-
-//     searchInput.addEventListener("focus", () => {
-//       searchInput.setAttribute("placeholder", "");
-//     });
-
-//     searchInput.addEventListener("blur", () => {
-//       searchInput.setAttribute("placeholder", originalPlaceholder);
-//     });
-//   }
-// });
-
 // Dark Mode / Light Mode Toggle
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggleCheckbox = document.getElementById('theme-toggle-checkbox');
